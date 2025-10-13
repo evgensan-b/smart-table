0459d0cdc2d8d28ad04e4553e9fbb5a0ba828428
EvgenSan <evgnesan@yandex.ru>
2025-10-13 23:27:12 +0530

    feat: Step 5 completed

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ import {initTable} from "./components/table.js";
 import { initPagination } from "./components/pagination.js";
 import { initSorting } from "./components/sorting.js";
 import { initFiltering } from "./components/filtering.js";
-
+import { initSearching } from "./components/searching.js";
 
 // Исходные данные используемые в render()
 const {data, ...indexes} = initData(sourceData);
@@ -40,6 +40,7 @@ function render(action) {
     let state = collectState(); // состояние полей из таблицы
     let result = [...data]; // копируем для последующего изменения
     // @todo: использование
+    result = applySearching(result, state, action);
     result = applyFiltering(result, state, action);
     result = applySorting(result, state, action);
     result = applyPagination(result, state, action);
@@ -51,7 +52,7 @@ function render(action) {
 const sampleTable = initTable({
     tableTemplate: 'table',
     rowTemplate: 'row',
-    before: ['header', 'filter'],
+    before: ['search', 'header', 'filter'],
     after: ['pagination']
 }, render);
 
@@ -76,6 +77,8 @@ const applySorting = initSorting([              // Нам нужно перед�
 const applyFiltering = initFiltering(sampleTable.filter.elements, {    // передаём элементы фильтра
     searchBySeller: indexes.sellers                                    // для элемента с именем searchBySeller устанавливаем массив продавцов
 });
+
+const applySearching = initSearching('search');  // Передаем имя поля для поиска
 
 
 const appRoot = document.querySelector('#app');
